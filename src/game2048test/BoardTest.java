034f2a70543be7a8886f53ebe007122a7eaf6865
Neils01 <iurryy@yahoo.com.br>
2015-12-07 14:57:00 -0300

    commitINicial

---
+++ src/game2048test/BoardTest.java
@@ -0,0 +1,19 @@
+package game2048test;
+
+import static org.junit.Assert.*;
+import game2048.Tile;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class BoardTest {
+	
+	@Test
+	@Category(game2048test.BoardTest.class)
+	public void text() {
+		Tile t1= new Tile(11);
+		assertEquals("11", t1.getValue().toString());
+	}
+
+}
