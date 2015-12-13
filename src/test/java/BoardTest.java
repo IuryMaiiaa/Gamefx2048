7489434fdcd43fa77d3b03e6f77129d304a0deb9
Neils01 <iurryy@yahoo.com.br>
2015-12-13 18:40:28 -0300

    commit de test jenkins build

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,22 @@
+package test.java;
+
+import static org.junit.Assert.*;
+import game2048.Tile;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+public class BoardTest {
+	
+	@Test
+	public void text() {
+		assertEquals("11","11");
+	}
+	
+	@Test
+	public void text1() {
+		assertEquals(2.1, 2.0);
+	}
+
+}
