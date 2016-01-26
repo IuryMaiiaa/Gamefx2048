4f4ea90da141fe4c016b7f200fb1f86aa48b38e8
Neils01 <iurryy@yahoo.com.br>
2016-01-26 15:31:45 -0300

    atualizando

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,7 +1,6 @@
 package test.java;
 
 import static org.junit.Assert.assertEquals;
-import main.java.Game2048;
 
 import org.junit.Test;
 
@@ -10,21 +9,5 @@ public class BoardTest {
 	@Test
 	public void text() {
 		assertEquals("11","11");
-	}
-	
-	@Test
-	public void novoTeste() {
-		Game2048 game = new Game2048();
-		game.main(null);
-		try {
-			game.init();
-			assertEquals(true,true);
-		} catch (Exception e) {
-			assertEquals(true,false);
-			e.printStackTrace();
-		}
-		
-		game.stop();
-		
 	}
 }
